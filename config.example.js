807b72c1c4b769b57efbe023c02ef6794653caeb
felipericarte <felipericartem@gmail.com>
2026-06-20 20:41:32 -0300

    Revert "docs: template config.example.js para produção"
    This reverts commit 87f7102e9e28ef2a4ab940be7404544adff562f2.

--- config.example.js
+++
@@ -1,5 +0,0 @@
-// Copie para config.js e ajuste antes de publicar a landing.
-window.COMERCIALCLOUD_CONFIG = {
-  trialUrl: 'https://app.seudominio.com.br/cadastro',
-  contactEmail: 'contato@seudominio.com.br'
-};
